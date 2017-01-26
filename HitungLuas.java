93ee284fb4d29f59a912c9d7cfa5b8ea7c7addb6
muhammadrikiF <muhammadrikiF@yahoo.com>
2017-01-26 23:12:23 +0700

    Update HitungLuas.java

--- HitungLuas.java
+++ HitungLuas.java
@@ -1,5 +1,15 @@
 public class HitungLuas {
   public static void main (String[] args){
-  
+    
+    int panjang = 7;
+    int lebar = 4;
+    float luas;
+    
+    System.out.println("Panjangnya="+panjang);
+    System.out.println("Lebarnya="+lebar);
+    
+    luas = (float)(panjang*lebar);
+    System.out.println("Luasnya adalah="+panjang*lebar);
+      
   }
   }
